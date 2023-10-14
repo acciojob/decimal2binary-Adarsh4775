623a8e3f7654357f0d9af90cf630eb1f3e924b19
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-14 18:44:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  // Special case for decimal 0
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
